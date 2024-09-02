1e51041c4b701c7119e6d884d6ddc459138b6b11
rachitjha09 <rachitjha09@gmail.com>
2024-09-02 19:28:13 -0400

    Add files via upload
    While and For loop practice

---
+++ Demo.java
@@ -0,0 +1,25 @@
+
+/**
+ * Demostrate Loops
+ *
+ * @author (Rachit Jha)
+ * @version (11/2/23)
+ */
+public class Demo
+{
+    public static void main(String[]args){
+        //while
+        //for
+        //for-each(not covered until arrays)
+        //do while(not covered)
+        
+        int i=0;
+        while(i<10){
+            System.out.println("i=" + i);
+            ++i;
+        }
+        for(int x=0; x<10; ++x){
+            System.out.println("x = " + x);
+        }
+    }
+}
